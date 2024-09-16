481abca92093aadd6a850f6f99d88322b988610c
tahayk3 <tahayk3@gmail.com>
2024-09-16 16:18:21 -0600

    listar muebles en el componente furniture

--- frontend/src/components/FurnitureList.jsx
+++ frontend/src/components/FurnitureList.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function FurnitureList(){
-    return(
-        <>
-            <h1>Componente FurnitureList</h1>
-        </>
-    );
-}
+const FurnitureList = () => {
+  const [furniture, setFurniture] = useState([]);
+
+  useEffect(() => {
+    const fetchFurniture = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/furniture/');
+        setFurniture(response.data);
+      } catch (error) {
+        console.error('Error fetching the furniture:', error);
+      }
+    };
+
+    fetchFurniture();
+  }, []);
+
+  return (
+    <div>
+      {furniture.results && furniture.results.length > 0 ? (
+        furniture.results.map((item) => (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <p>Price: ${item.price}</p>
+            <p>Stock: {item.stock}</p>
+          </div>
+        ))
+      ) : (
+        <p>No furniture available</p>
+      )}
+    </div>
+  );
+};
 
 export default FurnitureList;
