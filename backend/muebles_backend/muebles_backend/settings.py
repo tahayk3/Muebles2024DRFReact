1f86f92e7f3d32f2a22843e4105e8051b7207fe3
tahayk3 <tahayk3@gmail.com>
2024-09-19 11:28:41 -0600

    configurando tiempo de vida del token

--- backend/muebles_backend/muebles_backend/settings.py
+++ backend/muebles_backend/muebles_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -145,3 +146,19 @@ REST_FRAMEWORK = {
     )
 }
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),     
+    'ROTATE_REFRESH_TOKENS': False,                 
+    'BLACKLIST_AFTER_ROTATION': True,            
+    'ALGORITHM': 'HS256',                        
+    'SIGNING_KEY': SECRET_KEY,                       
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),               
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+}
