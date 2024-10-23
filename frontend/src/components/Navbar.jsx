bc084f809e9841e62642421b1b0bfb9d8066d8b9
tahayk3 <tahayk3@gmail.com>
2024-10-22 20:13:00 -0600

    modificando el navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import './Navbar.css';
 
 const Navbar = () =>{
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () =>{
+        setIsOpen(!isOpen);
+    }
+
     return(
-        <div className="navbar">
+        <>
+        <button className="menu-toggle" onClick={toggleMenu}>
+            {isOpen ? "✖": "☰"}
+        </button>
+        <div className={`navbar ${isOpen ? "open" : ""}`}>
             <img src="/logo.png" alt="" />
             <a href="/">Inicio</a>
             <a className="login-navbar" href="/login">Iniciar sesión</a>
         </div>
+        </>
+
     );
 };
 
